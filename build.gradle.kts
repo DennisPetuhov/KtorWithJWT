ee5516c3d1fc45ede3ae5316258dea03db9a7435
Dennis <kotlinkod@gmail.com>
2024-12-25 20:27:22 +0400

    fatjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
@@ -15,7 +14,8 @@ version = "0.0.1"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
 
-    val isDevelopment: Boolean = project.ext.has("development")
+    // Устанавливаем JVM-параметры для режима разработки
+    val isDevelopment: Boolean = project.hasProperty("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
@@ -24,13 +24,44 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-auth-jvm")
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-auth-jwt-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm")
-    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm")
+
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+tasks {
+    val fatJar = register<Jar>("fatJar") {
+        group = "build"
+        description = "Assembles a fat JAR file containing all runtime dependencies."
+
+        // Зависимости задачи
+        dependsOn("compileJava", "compileKotlin", "processResources")
+
+        // Имя файла JAR
+        archiveClassifier.set("standalone")
+
+        // Исключаем дубликаты (например, META-INF)
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+        // Добавляем классы и ресурсы
+        from(sourceSets.main.get().output)
+
+        // Включаем зависимости
+        dependsOn(configurations.runtimeClasspath)
+        from({
+            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+        })
+    }
+
+    // Добавляем задачу fatJar как зависимость от сборки
+    named("build") {
+        dependsOn(fatJar)
+    }
 }
